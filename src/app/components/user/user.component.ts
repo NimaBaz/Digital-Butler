3f9302463a3680c68799b86a6af67c850f3f350c
Juan Andreas <jandreas@ucsc.edu>
2017-11-19 02:02:36 -0800

    did most of the CSS componentizing

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -12,14 +12,6 @@ export class UserComponent implements OnInit {
   width:number;
   id:number;
   minimId:number;
-<<<<<<< HEAD
-
-=======
-        
-  monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
-  "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"
-  ];
->>>>>>> 903d9cc84f659b6e9dba3eb1f71e9826dbfbcc14
   constructor() {
     console.log('constructor ran..');
   }
@@ -28,47 +20,6 @@ export class UserComponent implements OnInit {
         //this.name = 'Hello, World!';
         //$("h1").html("hi");
         
-<<<<<<< HEAD
-=======
-        this.width = $("#A1").width();
-        console.log(this.width);
-        $("span").width = this.width;
-        var todayDate = new Date();
-        var month = this.monthNames[todayDate.getMonth()];
-        var day = todayDate.getDate();
-        var year = todayDate.getFullYear();
-        var hour = todayDate.getHours();
-        var minute = todayDate.getMinutes();
-        var min;
-        if(hour==0)
-            hour=12;
-        if(hour > 12) {
-            hour -= 12;
-            ampm = "pm";
-        }
-        if(minute < 10) { 
-            min = "0" + minute ;
-        } else {
-            min = minute;
-        }
-        var ampm = "am";
-        
-        document.getElementById('date').textContent = month + ", " + day + ", "+ year +" , "+ hour + ":" + min + " " + ampm;
-    }
-
-    openTab(evt, tabName) {
-        var i, tabcontent, tablinks;
-        tabcontent = document.getElementsByClassName("tabcontent");
-        for (i = 0; i < tabcontent.length; i++) {
-            tabcontent[i].style.display = "none";
-        }
-        tablinks = document.getElementsByClassName("tablinks");
-        for (i = 0; i < tablinks.length; i++) {
-            tablinks[i].className = tablinks[i].className.replace(" active", "");
-        }
-        document.getElementById(tabName).style.display = "block";
-        evt.currentTarget.className += " active";
->>>>>>> 903d9cc84f659b6e9dba3eb1f71e9826dbfbcc14
     }
 
     minimize(id_) { 
