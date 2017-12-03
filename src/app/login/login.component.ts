1b6b14a35a895960fd323a81298ad225e3e5bd35
Preston <pgong@ucsc.edu>
2017-12-03 10:44:27 -0800

    moved some modal css, added popup message for failure to login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -30,9 +30,16 @@ export class LoginComponent implements OnInit {
           this.router.navigate(['user']);
          
       }, err => {
-     
+          var small_modal = document.getElementById("errorPopup");
+          small_modal.style.display = "block";
           console.log("something is wrong");
       } 
     );
   }
+
+  closeModal(some_ID){
+    var modal = document.getElementById(some_ID);
+    modal.style.display = "none";
+  }
+
 }
